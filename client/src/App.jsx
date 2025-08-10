4547f07b46068b84a06f1ee7e8a62691c55a05e6
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-10 14:48:56 +0530

    Files under progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import WriteArticle from './pages/WriteArticle'
+import Blogtitles from './pages/Blogtitles'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path  = "/" element = {<Home />}/>
+        <Route path = "/ai" element = {<Layout />}>
+        <Route index element = {< DashBoard/>}/>
+        <Route path = 'write-article' element = {< WriteArticle/>}/>
+        <Route path = 'blog-title' element = {< BlogTitles/>}/>
+
+
+        </Route>
+      </Routes>
     </div>
   )
 }
