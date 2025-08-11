b8af288582a7f7718e4c906c27fe219e056e00dc
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-11 16:46:07 +0530

    Files under Progress

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     
     <Navbar />
     <Hero />
+    <AiTools />
     
     </>
   )
