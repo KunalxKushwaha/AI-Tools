083d59976b2d5b114c2246151ec8ef487af07f7a
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-11 16:14:50 +0530

    Files under Progress

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import bgImage from '../assets/gradientBackground.png'
+import { useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
+  const navigate = useNavigate()
   return (
      <div   style={{ backgroundImage: `url(${bgImage})` }}
  className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
+
+      <div className='text-center mb-6'>
+        <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl
+        font-semibold mx-auto leading-[1.2]'>Create Amazing Content <br/> with <span className='text-[var(--primary-color)]'>AI Tools</span></h1>
+        <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto
+        max-sm:text-xs text-gray-600'>Transform your content creation with our suite of Premium Tools.
+          Write Articles, Generate Images, and much more with ease.
+        </p>
+      </div>
+
+      <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
+        <button className='bg-[var(--primary-color)] text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>Start Creating Now</button>
+        <button onClick = {()=> navigate('/ai')} className='bg-white px-10 py-3 rounded-lg border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer '>Watch Demo</button>
+      </div>
+
+      <div className='flex items-center gap-4 text-gray-600 mt-8 mx-auto'>
+        <img src={assets.user_group} alt="" className='h-8'/>Trusted by over 100,000 users
+      </div>
       
     </div>
     
