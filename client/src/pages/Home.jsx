4d89863d897372fa0cccc8232f4cf8a093004972
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-11 18:27:50 +0530

    Files under Progress

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonials from '../components/Testimonials'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 
 const Home = () => {
@@ -13,6 +15,8 @@ const Home = () => {
     <Hero />
     <AiTools />
     <Testimonials />
+    <Plan />
+    <Footer />
     
     </>
   )
