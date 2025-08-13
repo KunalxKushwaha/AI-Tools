101cb883941b861d3b30378f06649f73384995fd
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-13 15:06:05 +0530

    Blog Page Chl rha

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -52,8 +52,8 @@ const Blogtitles = () => {
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
 
         <div className='flex items-center gap-3'>
-          <Edit className='w-5 h-5 text-[#$A7AFF]'/>
-          <h1 className='text-xl font-semibold'>Generated Article</h1>
+          <Edit className='w-5 h-5 text-[#8E37EB]'/>
+          <h1 className='text-xl font-semibold'>Generated Titles</h1>
 
         </div>
         <div className='flex-1 flex justify-center items-center'>
