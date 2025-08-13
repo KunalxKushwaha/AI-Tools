5b89dad6071ac1614b0797213a7d93fccb3e7be4
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-13 15:29:19 +0530

    Image Page Done

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,10 +1,74 @@
+import { Hash, Sparkles, Image} from 'lucide-react'
 import React from 'react'
+import {useState} from 'react'
 
 const GenerateImages = () => {
+
+   const imageStyle = [
+        'Realistic', 'Ghibli', 'Anime', 'Cartoon', 'Fantasy', '3D Effect', 'Portrait'
+        
+      ]
+    
+      const [selectedStyle, setSelectedStyle] = useState('Realistic')
+    const [input, setInput] = useState('')
+    const [publish, setPublish] = useState(false)
+    const onSubmitHandler = async(e)=> {
+      e.preventDefalut();
+    }
+
   return (
-    <div>
-      GenerateImages
-    </div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+    
+          {/*left Col*/ }
+          <form  onSubmit = {onSubmitHandler}className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+            <div className='flex item-center gap-3'>
+              <Sparkles className='w-6 text-[#00AD25]'/>
+              <h1 className='text-xl font-semibold'>AI Image Generator</h1>
+            </div>
+            <p className='mt-6 text-sm font-medium'>Describe Your Image</p>
+            <textarea onChange={(e)=> setInput(e.target.value)} value={input} rows={4} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 hover:border-[#65ADFF] shadow' placeholder='Describe About your Image to be Generated' required />
+            <p className='mt-4 text-sm font-medium'>Style</p>
+    
+            <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+              {
+                imageStyle.map((item)=> (
+                  <span 
+                  onClick={()=> setSelectedStyle(item)}
+                   key={item} 
+                  className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle=== item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`}>
+                    {item}
+                    </span>
+                ))
+              }
+            </div>
+            <br/>
+    
+            <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+              <Image className='w-5'/>
+              Generate Image
+            </button>
+    
+            
+          </form>
+          {/*Right Col*/ }
+          <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+    
+            <div className='flex items-center gap-3'>
+              <Image className='w-5 h-5 text-[#00AD25]'/>
+              <h1 className='text-xl font-semibold'>Generated Images</h1>
+    
+            </div>
+            <div className='flex-1 flex justify-center items-center'>
+              <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                <Image className='w-9 h-9'/>
+                <p className='justify-center item-center flex'>Enter Your Topic and Click "Generate Image" to get Started </p>
+              </div>
+    
+            </div>
+           
+          </div>
+    
+        </div>
   )
 }
 
