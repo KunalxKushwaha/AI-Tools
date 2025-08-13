70b32535dfefef32361ba5a72845225d6ac143dc
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-13 14:17:40 +0530

    Article Section Chl rha

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,7 +1,18 @@
 import { Sparkles } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const WriteArticle = () => {
+
+  const articleLenght = [
+    {lenght:800, text :'Short (500-800 words)'},
+    {lenght:1200, text :'Medium (800-1200 words)'},
+    {lenght:1600, text :'Long (1200+ words)'},
+    
+  ]
+
+  const [selectedLenght, setSelectedLenght] = useState(articleLenght[0])
+const [input, setInput] = useState('')
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
 
@@ -11,7 +22,24 @@ const WriteArticle = () => {
           <Sparkles className='w-6 text-[#4A7AFF]'/>
           <h1 className='text-xl font-semibold'>Article Configuration</h1>
         </div>
+        <p className='mt-6 text-sm font-medium'>Article Topic</p>
+        <input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The Future of Artificial Intelligence is...' required />
+        <p className='mt-4 text-sm font-medium'>Article Lenght</p>
 
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {
+            articleLenght.map((item,index)=> (
+              <span 
+              onClick={()=> setSelectedLenght(item)}
+               key={index} 
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedLenght.text === item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300'}`}>
+                {item.text}
+                </span>
+            ))
+          }
+        </div>
+
+        
       </form>
       {/*Right Col*/ }
       <div>
