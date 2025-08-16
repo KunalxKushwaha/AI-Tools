dfc86243d06e421f75e4e2863fdbb310bbd512d1
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-16 14:41:55 +0530

    Backend Started

--- Server/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {auth} from "../Middlewares/auth.js"
-import { generateArticle } from "../controllers/aiController.js";
-
-const aiRouter = express.Router();
-
-aiRouter.post('/generate-article', auth, generateArticle)
-
-export default aiRouter
