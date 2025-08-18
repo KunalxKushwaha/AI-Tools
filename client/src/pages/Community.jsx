0ff87e55a9c7387570ac350733c2cfee92064c58
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-18 16:25:04 +0530

    Finished✌

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,14 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import {useUser} from '@clerk/clerk-react'
-import { dummyPublishedCreationData } from '../assets/assets'
 import { Heart } from 'lucide-react'
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+ 
 
 const Community = () => {
 
 const [creations, setCreation] = useState([])
 const {user} = useUser()
+const [loading, setLoading] = useState(true)
+const {getToken} = useAuth()
 const fetchCreations = async () =>{
-  setCreation(dummyPublishedCreationData)
+  try {
+    const {data} = await axios.get('/api/ai/get-published-creations', {
+      headers: {
+        Authorization: `Bearer ${await getToken()}`
+      }
+    })
+    if(data.success) {
+      setCreation(data.creations)
+    } else {
+      toast.error(data.message)
+    }
+  } catch (error) {
+    toast.error(error.message)
+  }
+  setLoading(false)
+}
+
+const imageLikeToggle = async (id)=> {
+  try {
+     const {data} = await axios.post('/api/ai/toggle-like-creations', {id}, {
+      headers: {
+        Authorization: `Bearer ${await getToken()}`
+      }
+    })
+
+    if(data.success) {
+      toast.success(data.message)
+      await fetchCreations()
+
+    } else {
+      toast.error(data.message)
+    }
+
+
+    
+  } catch (error) {
+          toast.error(error.message)
+
+    
+  }
+
 }
 
 useEffect(()=> {
@@ -17,8 +64,8 @@ useEffect(()=> {
   }
 }, [user])
 
-  return (
-    <div className='flex-1 h-full flex flex-col gap-4 p-6'>
+  return !loading ? (
+    <div className='flex-1 h-full flex flex-col gap-4 p-6 items-center justify-center'>
       Creations
       <div className='bg-white h-full w-full rounded-xl overflow-y-scroll'>
         {
@@ -30,7 +77,7 @@ useEffect(()=> {
                 <p className='text-sm hidden group-hover:block'>{creation.prompt}</p>
                 <div className='flex gap-1 items-center'>
                   <p>{creation.likes.length}</p>
-                  <Heart className= {`min-w-5 h-5 hover:scale-110 cursor-pointer ${creation.likes.includes(user.id) ? 'fill-red-500 text-red-600': 'text-white'}`}/>
+                  <Heart onClick={()=> imageLikeToggle(creation.id)} className= {`min-w-5 h-5 hover:scale-110 cursor-pointer ${creation.likes.includes(user.id) ? 'fill-red-500 text-red-600': 'text-white'}`}/>
                 </div>
               </div>
             </div>
@@ -39,6 +86,10 @@ useEffect(()=> {
  
       </div>
     </div>
+  ) : (
+      <div className='flex justify-center items-center h-full'>
+        <span className='w-10 h-10 my-1 rounded-full border-3 border-[#5044e5] border-t-transparent animate-spin items-center justify-ceter flex'></span>
+      </div>
   )
 }
 
