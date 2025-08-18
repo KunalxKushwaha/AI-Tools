d3db933014922495651267c04134be14b252cc2e
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-18 12:10:02 +0530

    Finished✌

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     
     <Navbar />
     <Hero />
-    <div className="marquee-inner flex will-change-transform min-w-[200%]" style={{ animationDuration: '30s' }}>
+    {/* <div className="marquee-inner flex will-change-transform min-w-[200%]" style={{ animationDuration: '30s' }}>
         <div className="flex">
           <img alt="slack" className="w-full h-full object-cover mx-6" draggable="false" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/companyLogo/slack.svg" />
           <img alt="framer" className="w-full h-full object-cover mx-6" draggable="false" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/companyLogo/framer.svg" />
@@ -30,7 +30,7 @@ const Home = () => {
           <img alt="instagram" className="w-full h-full object-cover mx-6" draggable="false" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/companyLogo/instagram.svg" />
           <img alt="facebook" className="w-full h-full object-cover mx-6" draggable="false" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/companyLogo/facebook.svg" />
         </div>
-      </div>
+      </div> */}
     <AiTools />
     <Testimonials />
     <Plan />
